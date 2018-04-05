9ee142620872af5ac8583d079ef4f6ca3476793c
Thaigo_Freitas <tmfreitas@outlook.com>
2018-04-05 01:05:17 -0300

    criando regras de permissoes

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,10 @@
 module Api
   module V1
-     class ApiController < ApplicationController
+    class ApiController < ApplicationController
       include Knock::Authenticable
-        #> Métodos globais
-     end
+      include CanCan::ControllerAdditions
+    
+      #> Métodos globais
+    end
   end
 end
